618b1c472d42a6b32d937f4ffe22e0de73ec66c3
Vitoriano Ferrero Martin Jr <victorianojr@gmail.com>
2024-10-15 09:48:20 -0300

    Add files via upload

--- Classe1.java
+++ Classe1.java
@@ -4,10 +4,10 @@ public class Classe1 {
     public static void main(String[] args) {
         Scanner scnInputUsuario = new Scanner(System.in); // Input de entrada via terminal do usuario
         System.out.println("Olá! Bem vindo a calculadora de JaVa. Digite o primeiro número a ser somado e tecle: \"Enter\"");
-        int intInputUsuario1 = scnInputUsuario.nextInt(); // captura o que o usuario registrar no terminal
+        float fltInputUsuario1 = scnInputUsuario.nextFloat(); // captura o que o usuario registrar no terminal
         System.out.println("Agora digite o segundo número a ser somado e tecle: \"Enter\"");
-        int intInputUsuario2 = scnInputUsuario.nextInt(); // captura o que o usuario registrar no terminal
-        System.out.println("O resultado da soma é: " + (intInputUsuario1 + intInputUsuario2));
+        float fltInputUsuario2 = scnInputUsuario.nextFloat(); // captura o que o usuario registrar no terminal
+        System.out.println("O resultado da soma é: " + (fltInputUsuario1 + fltInputUsuario2));
         scnInputUsuario.close();
     }
 }
