a8b7bbbc5a15290c42559242d9e1c13a0007f2c9
Vitoriano Ferrero Martin Jr <victorianojr@gmail.com>
2024-10-18 11:59:00 -0300

    Add files via upload

--- src/controller/TelaDeLoginController.java
+++ src/controller/TelaDeLoginController.java
@@ -4,13 +4,11 @@ import model.*;
 import view.*;
 
 public class TelaDeLoginController extends TelaDeLoginView {
-    public static String[] retornoUsuario = {
-        "Conectado com sucesso!", // resposta 0
-        "Login ou senha incorretos!", // resposta 1
-        "Não foi possível encontrar o login e/ou senha digitados/informados! Por favor, verifique e tente novamente." // resposta 2
-    };
+    public static void notificarUsuario(String textoNotificacao) {
+        lblNotificacoes.setText(setHtmlFormat(textoNotificacao));
+    }
 
-    public static int logarController(String login, String senha) {
-        return TelaDeLoginModel.logarModel(login, senha);
+    public static void logarController(String login, String senha) {
+        TelaDeLoginModel.logarModel(login, senha);
     }
 }
--- src/model/TelaDeLoginModel.java
+++ src/model/TelaDeLoginModel.java
@@ -1,25 +1,27 @@
 package model;
 
-// import controller.*;
+import controller.*;
 
 import java.sql.*;
 
+import controller.TelaDeLoginController;
+
 public class TelaDeLoginModel {
-    public static int logarModel(String login, String senha) {
+    public static void logarModel(String login, String senha) {
         try {
             Connection conexao = MySQLConnector.conectar();
             String strSqlLogin = "select * from `db_senac`.`tbl_senac` where `email` = '" + login + "' and `senha` = '" + senha + "';";
             Statement stmSqlLogin = conexao.createStatement();
             ResultSet rstSqlLogin = stmSqlLogin.executeQuery(strSqlLogin);
             if (rstSqlLogin.next()) { // Verifica a existência de um usuário com determinado e-mail em um banco de dados.
-                return 0;
+                TelaDeLoginController.notificarUsuario("Conectado com sucesso!!!");
             } else { // Verifica se o login é válido. Caso não seja, ele permite que o usuário realize um cadastro. Os dados do cadastro são inseridos em um banco de dados e uma mensagem de confirmação é exibida para o usuário.
-                stmSqlLogin.close(); // Encerra a comunicação com o banco de dados que estava sendo utilizada para realizar as operações relacionadas ao envio de e-mails.
-                return 1;
+                TelaDeLoginController.notificarUsuario("Login e/ou senha não encontrado! Por favor, verifique e tente novamente.");
             }
+            stmSqlLogin.close(); // Encerra a comunicação com o banco de dados que estava sendo utilizada para realizar as operações relacionadas ao envio de e-mails.
         } catch (Exception e) { // Se der algum problema ao cadastrar, mostrar uma mensagem de erro para o usuário e imprimir uma mensagem feinha para o programador.
+            TelaDeLoginController.notificarUsuario("Houve um problema e não será possível realizar o login agora. Por favor, tente novamente mais tarde.");
             System.err.println("Erro: " + e);
-            return 2;
         }
     }
 }
